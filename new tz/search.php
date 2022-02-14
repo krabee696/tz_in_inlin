b3d7edac323ae4cc12baf690444e483617eddc24
relam696 <42041281+krabee696@users.noreply.github.com>
2022-02-14 13:26:22 +0700

    Add files via upload

--- new tz/search.php
+++ new tz/search.php
@@ -10,7 +10,7 @@ table, th, td {border: 1px solid black;border-collapse: collapse;}
 <body>
 <form action="" method="GET">
         <input type="text" name="query" />
-		<input type="submit" name="submit" value="Search|Найти" />
+		<input type="submit" name="submit" value="Найти" />
 </form>
 <?php
 require 'connect.php';
@@ -18,10 +18,11 @@ require 'connect.php';
 if(isset($_GET['query'])) {
 	$query = $_GET['query'];
 	if(strlen($query) >= 3){
-        $sql = "SELECT p.title, c.body FROM post p INNER JOIN comment c ON c.postid = p.id WHERE c.body LIKE ?";
+        $sql = "SELECT p.title, c.body FROM posts p INNER JOIN comments c ON c.postid = p.id WHERE c.body LIKE ?";
+        $stmt = $connect -> prepare($sql);
 		$stmt = $connect -> prepare($sql);
 		$query = "%" . $query . "%";
-		$stmt -> bind_param("s", $query);
+		$stmt -> bind_param('s', $query);
 		$stmt -> execute();
 		$arr = $stmt -> get_result()->fetch_all(MYSQLI_ASSOC);
 		$stmt -> close(); }
@@ -40,6 +41,7 @@ if (isset($arr)){
 echo "</table>";
 }
 }
+
 ?>
 </body>
 </html>
